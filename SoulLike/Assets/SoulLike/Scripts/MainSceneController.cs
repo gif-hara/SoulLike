6913248e5ee4a4f4b245775d0485baeedb325094
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 06:33:20 +0900

    MainSceneControllerクラスにプレイヤーPrefabとスポーンポイントのSerializeFieldを追加

--- SoulLike/Assets/SoulLike/Scripts/MainSceneController.cs
+++ SoulLike/Assets/SoulLike/Scripts/MainSceneController.cs
@@ -1,8 +1,19 @@
+using SoulLike.ActorControllers;
 using UnityEngine;
 
 namespace SoulLike
 {
     public class MainSceneController : MonoBehaviour
     {
+        [field: SerializeField]
+        private Actor playerPrefab;
+
+        [field: SerializeField]
+        private Transform spawnPoint;
+
+        void Start()
+        {
+            playerPrefab.Spawn(spawnPoint.position, spawnPoint.rotation);
+        }
     }
 }
