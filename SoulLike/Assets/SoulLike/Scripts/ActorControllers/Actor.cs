9d09d12adebf1104c647ab8e683cca9d207c3fc6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 07:59:03 +0900

    ActorクラスにBrainControllerプロパティを追加し、Spawnメソッドで初期化するように変更

--- SoulLike/Assets/SoulLike/Scripts/ActorControllers/Actor.cs
+++ SoulLike/Assets/SoulLike/Scripts/ActorControllers/Actor.cs
@@ -17,13 +17,18 @@ namespace SoulLike.ActorControllers
 
         public ActorTimeController TimeController { get; private set; }
 
+        public ActorBrainController BrainController { get; private set; }
+
         public Actor Spawn(Vector3 position, Quaternion rotation)
         {
             var actor = Instantiate(this, position, rotation);
             actor.TimeController = new ActorTimeController(actor);
             actor.MovementController = new ActorMovementController();
             actor.AnimationController = new ActorAnimationController(actor);
+            actor.BrainController = new ActorBrainController(actor);
+
             actor.MovementController.Setup(actor, actor.openCharacterController);
+
             return actor;
         }
     }
