73c51dd664e5445f023c73e15c387cc370d81763
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 08:10:23 +0900

    PlayerクラスにPlayerInputを追加し、Attachメソッドで移動入力を処理するように変更

--- SoulLike/Assets/SoulLike/Scripts/Brains/Player.cs
+++ SoulLike/Assets/SoulLike/Scripts/Brains/Player.cs
@@ -1,13 +1,30 @@
 using System.Threading;
+using R3;
+using R3.Triggers;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace SoulLike.ActorControllers.Brains
 {
     public sealed class Player : IActorBrain
     {
+        private readonly PlayerInput playerInput;
+
+        public Player(PlayerInput playerInput)
+        {
+            this.playerInput = playerInput;
+        }
+
         public void Attach(Actor actor, CancellationToken cancellationToken)
         {
-            Debug.Log("Player brain attached to actor: " + actor.name);
+            actor.UpdateAsObservable()
+                .Subscribe((this, actor), static (_, t) =>
+                {
+                    var (@this, actor) = t;
+                    var moveVelocity = @this.playerInput.actions["Move"].ReadValue<Vector2>();
+                    Debug.Log($"Player Move: {moveVelocity}");
+                })
+                .RegisterTo(cancellationToken);
         }
     }
 }
