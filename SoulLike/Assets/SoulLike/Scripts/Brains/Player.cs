e0357bb52a5786530c6ec7d884d5dbc9b24350bd
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 08:15:33 +0900

    PlayerクラスにCameraを追加し、移動入力をカメラの方向に基づいて処理するように変更

--- SoulLike/Assets/SoulLike/Scripts/Brains/Player.cs
+++ SoulLike/Assets/SoulLike/Scripts/Brains/Player.cs
@@ -9,10 +9,12 @@ namespace SoulLike.ActorControllers.Brains
     public sealed class Player : IActorBrain
     {
         private readonly PlayerInput playerInput;
+        private readonly Camera camera;
 
-        public Player(PlayerInput playerInput)
+        public Player(PlayerInput playerInput, Camera camera)
         {
             this.playerInput = playerInput;
+            this.camera = camera;
         }
 
         public void Attach(Actor actor, CancellationToken cancellationToken)
@@ -21,8 +23,17 @@ namespace SoulLike.ActorControllers.Brains
                 .Subscribe((this, actor), static (_, t) =>
                 {
                     var (@this, actor) = t;
-                    var moveVelocity = @this.playerInput.actions["Move"].ReadValue<Vector2>();
-                    Debug.Log($"Player Move: {moveVelocity}");
+                    var moveInput = @this.playerInput.actions["Move"].ReadValue<Vector2>();
+                    // カメラの方向に合わせて移動ベクトルを変換
+                    var camTransform = @this.camera.transform;
+                    var forward = camTransform.forward;
+                    var right = camTransform.right;
+                    forward.y = 0;
+                    right.y = 0;
+                    forward.Normalize();
+                    right.Normalize();
+                    var moveVelocity = (right * moveInput.x + forward * moveInput.y);
+                    actor.MovementController.Move(moveVelocity);
                 })
                 .RegisterTo(cancellationToken);
         }
