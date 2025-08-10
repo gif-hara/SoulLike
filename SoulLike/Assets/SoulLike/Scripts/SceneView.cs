8fbefdaaf559356ad818180c64536b29ce2e183d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 09:07:37 +0900

    SceneViewクラスを新規作成し、LocatorHolderとAnimatorをシリアライズ可能なプロパティとして追加

---
+++ SoulLike/Assets/SoulLike/Scripts/SceneView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace SoulLike
+{
+    public class SceneView : MonoBehaviour
+    {
+        [field: SerializeField]
+        public LocatorHolder LocatorHolder { get; private set; }
+
+        [field: SerializeField]
+        public Animator Animator { get; private set; }
+    }
+}
