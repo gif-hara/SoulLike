9f371f06165fb27a9cda5c8b861228ef0099da5b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 15:26:14 +0900

    ActorAttackクラスを新規作成し、IActorAbilityインターフェースを実装

---
+++ SoulLike/Assets/SoulLike/Scripts/ActorControllers/Abilities/ActorAttack.cs
@@ -0,0 +1,15 @@
+using System.Threading;
+using SoulLike.ActorControllers.Brains;
+using UnityEngine;
+
+namespace SoulLike.ActorControllers.Abilities
+{
+    public sealed class ActorAttack : IActorAbility
+    {
+
+        public void Activate(Actor actor)
+        {
+        }
+
+    }
+}
