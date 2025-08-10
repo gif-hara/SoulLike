3d1e7a9edf7a07794bad2eba14f9ba395a394bde
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 11:00:33 +0900

    ActorAnimationクラスの未使用コードを削除し、構造を整理

--- SoulLike/Assets/SoulLike/Scripts/ActorControllers/Abilities/ActorAnimation.cs
+++ SoulLike/Assets/SoulLike/Scripts/ActorControllers/Abilities/ActorAnimation.cs
@@ -1,29 +1,67 @@
-using System;
-using System.Threading;
-using Cysharp.Threading.Tasks;
-using R3;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace SoulLike.ActorControllers.Abilities
 {
-    public class ActorAnimation
+    public sealed class ActorAnimation : IActorAbility
     {
-        private readonly Actor actor;
+        private Animator animator;
 
-        public ActorAnimation(Actor actor)
+        private readonly AnimatorParameter.DictionaryList animatorParameters = new();
+
+        public static class Parameter
         {
-            this.actor = actor;
-            // actor.TimeController.UpdatedTimeScale
-            //     .Subscribe((actor, simpleAnimation), static (_, t) =>
-            //     {
-            //         var (actor, simpleAnimation) = t;
-            //         foreach (var state in simpleAnimation.GetStates())
-            //         {
-            //             state.speed = actor.TimeController.Time.totalTimeScale;
-            //         }
-            //     })
-            //     .RegisterTo(actor.destroyCancellationToken);
+            public const string MoveSpeed = "MoveSpeed";
+        }
+
+        public void Activate(Actor actor)
+        {
+            actor.TryGetComponent<SceneView>(out var sceneView);
+            Assert.IsNotNull(sceneView, $"{nameof(SceneView)} is not assigned in {actor.name}.");
+            animator = sceneView.Animator;
+            Assert.IsNotNull(animator, $"{nameof(Animator)} is not assigned in {actor.name}.");
+        }
+
+        private AnimatorParameter GetParameter(string parameterName)
+        {
+            if (animatorParameters.TryGetValue(parameterName, out var animatorParameter))
+            {
+                return animatorParameter;
+            }
+
+            animatorParameter = new AnimatorParameter(parameterName);
+            animatorParameters.Add(animatorParameter);
+            return animatorParameter;
+        }
+
+        public void SetBool(string parameterName, bool value)
+        {
+            var animatorParameter = GetParameter(parameterName);
+            animator.SetBool(animatorParameter.Hash, value);
+        }
+
+        public void SetTrigger(string parameterName)
+        {
+            var animatorParameter = GetParameter(parameterName);
+            animator.SetTrigger(animatorParameter.Hash);
+        }
+
+        public void SetFloat(string parameterName, float value)
+        {
+            var animatorParameter = GetParameter(parameterName);
+            animator.SetFloat(animatorParameter.Hash, value);
+        }
+
+        public void SetInt(string parameterName, int value)
+        {
+            var animatorParameter = GetParameter(parameterName);
+            animator.SetInteger(animatorParameter.Hash, value);
+        }
+
+        public void ResetTrigger(string parameterName)
+        {
+            var animatorParameter = GetParameter(parameterName);
+            animator.ResetTrigger(animatorParameter.Hash);
         }
     }
 }
