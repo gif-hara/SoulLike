66958af817d7cd3cdc8ede6cae7940d0cbc3a44e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 15:29:40 +0900

    ActorAttackクラスに攻撃処理を追加し、ActivateメソッドでActorAnimationを取得するように修正

--- SoulLike/Assets/SoulLike/Scripts/ActorControllers/Abilities/ActorAttack.cs
+++ SoulLike/Assets/SoulLike/Scripts/ActorControllers/Abilities/ActorAttack.cs
@@ -6,10 +6,17 @@ namespace SoulLike.ActorControllers.Abilities
 {
     public sealed class ActorAttack : IActorAbility
     {
+        private ActorAnimation actorAnimation;
 
         public void Activate(Actor actor)
         {
+            actorAnimation = actor.GetAbility<ActorAnimation>();
         }
 
+        public bool TryAttack()
+        {
+            actorAnimation.SetTrigger(ActorAnimation.Parameter.Attack);
+            return true;
+        }
     }
 }
