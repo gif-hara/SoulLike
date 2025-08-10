ba6d58a8e21e071a352564b84d7e1085b7e281d9
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 10:34:08 +0900

    PlayerクラスにPlayerSpecを追加し、移動速度と回転速度を設定

--- SoulLike/Assets/SoulLike/Scripts/Brains/Player.cs
+++ SoulLike/Assets/SoulLike/Scripts/Brains/Player.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using R3;
 using R3.Triggers;
 using SoulLike.ActorControllers.Abilities;
+using SoulLike.MasterDataSystem;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,18 +14,22 @@ namespace SoulLike.ActorControllers.Brains
 
         private readonly Camera camera;
 
+        private readonly PlayerSpec playerSpec;
+
         private ActorMovement movementController;
 
-        public Player(PlayerInput playerInput, Camera camera)
+        public Player(PlayerInput playerInput, Camera camera, PlayerSpec playerSpec)
         {
             this.playerInput = playerInput;
             this.camera = camera;
+            this.playerSpec = playerSpec;
         }
 
         public void Attach(Actor actor, CancellationToken cancellationToken)
         {
             actor.AddAbility<ActorTime>();
             movementController = actor.AddAbility<ActorMovement>();
+            movementController.SetRotationSpeed(playerSpec.RotateSpeed);
             actor.UpdateAsObservable()
                 .Subscribe((this, actor), static (_, t) =>
                 {
@@ -38,7 +43,15 @@ namespace SoulLike.ActorControllers.Brains
                     forward.Normalize();
                     right.Normalize();
                     var moveVelocity = right * moveInput.x + forward * moveInput.y;
+                    moveVelocity *= @this.playerSpec.MoveSpeed;
                     @this.movementController.Move(moveVelocity);
+
+                    // 移動入力がある場合、移動方向に向く
+                    if (moveVelocity.sqrMagnitude > 0.0001f)
+                    {
+                        var targetRotation = Quaternion.LookRotation(moveVelocity, Vector3.up);
+                        @this.movementController.Rotate(targetRotation);
+                    }
                 })
                 .RegisterTo(cancellationToken);
         }
