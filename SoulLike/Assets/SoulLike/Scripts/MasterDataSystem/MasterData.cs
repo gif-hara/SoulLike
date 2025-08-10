fe7f070ef9a074b3865b2aae1a92156788c98527
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-10 10:11:21 +0900

    MasterDataクラスと関連するメタファイルを新規作成

---
+++ SoulLike/Assets/SoulLike/Scripts/MasterDataSystem/MasterData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace SoulLike.MasterDataSystem
+{
+    [CreateAssetMenu(fileName = "MasterData", menuName = "SoulLike/MasterData")]
+    public class MasterData : ScriptableObject
+    {
+    }
+}
