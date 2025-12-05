74aa29c115942a0fbe782f80a95b865107f1b2f9
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-05 17:35:56 +0900

    SubscribeEndAttackクラスを新規作成し、攻撃終了イベントを処理する機能を追加

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/WeaponActions/SubscribeEndAttack.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using R3;
+using SoulLike.ActorControllers;
+using TNRD;
+using UnityEngine;
+
+namespace SoulLike.WeaponActions
+{
+    [Serializable]
+    public sealed class SubscribeEndAttack : IWeaponAction
+    {
+        [SerializeField]
+        private int attackId;
+
+#if UNITY_EDITOR
+        [ClassesOnly]
+#endif
+        [SerializeField]
+        private List<SerializableInterface<IWeaponAction>> actions = new();
+
+        public void Invoke(Weapon weapon, Actor actor, CancellationToken scope)
+        {
+            actor.Event.Broker.Receive<ActorEvent.EndAttack>()
+                .Where(attackId, static (x, attackId) => x.AttackId == attackId)
+                .Subscribe((this, weapon, actor, scope), static (_, t) =>
+                {
+                    var (@this, weapon, actor, scope) = t;
+                    foreach (var actionInterface in @this.actions)
+                    {
+                        actionInterface.Value.Invoke(weapon, actor, scope);
+                    }
+                })
+                .RegisterTo(scope);
+        }
+    }
+}
