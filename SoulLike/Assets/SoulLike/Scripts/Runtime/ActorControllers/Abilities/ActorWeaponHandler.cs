0af877ba90cead8551a25d9ac800a2639d10aeb5
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-06 20:29:19 +0900

    ActorWeaponHandlerクラスから未使用のコードを削除

--- SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Abilities/ActorWeaponHandler.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Abilities/ActorWeaponHandler.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Threading;
 using R3;
-using UnityEngine;
 
 namespace SoulLike.ActorControllers.Abilities
 {
@@ -9,21 +6,13 @@ namespace SoulLike.ActorControllers.Abilities
     {
         private Actor actor;
 
-        private ActorMovement actorMovement;
-
-        private ActorAnimation actorAnimation;
-
         public readonly ReactiveProperty<bool> CanAttack = new(true);
-
-        private IDisposable attackExitProcess;
 
         private Weapon currentWeapon;
 
         public void Activate(Actor actor)
         {
             this.actor = actor;
-            actorMovement = actor.GetAbility<ActorMovement>();
-            actorAnimation = actor.GetAbility<ActorAnimation>();
         }
 
         public void CreateWeapon(Weapon weaponPrefab)
@@ -45,29 +34,6 @@ namespace SoulLike.ActorControllers.Abilities
             }
 
             currentWeapon.InvokeBasicAttack();
-
-            // attackExitProcess?.Dispose();
-            // attackExitProcess = null;
-            // actorAnimation.SetInteger(ActorAnimation.Parameter.AttackId, attackId);
-            // actorAnimation.SetTrigger(ActorAnimation.Parameter.Attack);
-            // CanAttack.Value = false;
-            // var currentAttackId = attackId;
-            // attackId++;
-            // actorAnimation.UpdateAnimator();
-            // attackExitProcess = actorAnimation.OnStateExitAsObservable()
-            //     .Subscribe((this, currentAttackId), static (x, t) =>
-            //     {
-            //         var (@this, currentAttackId) = t;
-            //         if (x.StateInfo.IsName(ActorAnimation.Parameter.GetAttackStateName(@this.weaponId, currentAttackId)))
-            //         {
-            //             @this.actorMovement.CanMove.Value = true;
-            //             @this.CanAttack.Value = true;
-            //             @this.attackId = 1;
-            //             @this.attackExitProcess?.Dispose();
-            //             @this.attackExitProcess = null;
-            //             Debug.Log($"Attack finished");
-            //         }
-            //     });
             return true;
         }
     }
