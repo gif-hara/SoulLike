fe66911899acd747154d583154be209477f254a4
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-06 20:37:03 +0900

    Layerクラスを新規作成し、ゲームオブジェクトのレイヤー定数を定義

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/Layer.cs
@@ -0,0 +1,15 @@
+namespace SoulLike
+{
+    public static class Layer
+    {
+        public const int Default = 0;
+        public const int TransparentFX = 1;
+        public const int IgnoreRaycast = 2;
+        public const int Water = 4;
+        public const int UI = 5;
+        public const int Player = 6;
+        public const int Enemy = 7;
+        public const int PlayerWeapon = 8;
+        public const int EnemyWeapon = 9;
+    }
+}
