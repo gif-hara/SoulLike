778d735cd61638d7d30fb4c6483bb5efbec8e94e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-07 00:36:40 +0900

    WorldCameraControllerにロックオンカメラのターゲット設定機能を追加

--- SoulLike/Assets/SoulLike/Scripts/Runtime/WorldCameraController.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/WorldCameraController.cs
@@ -11,9 +11,23 @@ namespace SoulLike
         [field: SerializeField]
         private CinemachineCamera defaultCamera;
 
+        [SerializeField]
+        private CinemachineCamera lockOnCamera;
+
         public void SetDefaultCameraTarget(Transform target)
         {
             defaultCamera.Target.TrackingTarget = target;
+        }
+
+        public void SetLockOnCameraTarget(Transform follow, Transform lookAt)
+        {
+            lockOnCamera.Target.TrackingTarget = follow;
+            lockOnCamera.Target.LookAtTarget = lookAt;
+        }
+
+        public void SetActiveLockOnCamera(bool isActive)
+        {
+            lockOnCamera.gameObject.SetActive(isActive);
         }
     }
 }
