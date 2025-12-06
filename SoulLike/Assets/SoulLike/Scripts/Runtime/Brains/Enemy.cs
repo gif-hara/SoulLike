da4408d83a79c9740a90a711b954e86d33bee6de
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-06 20:42:57 +0900

    Enemyクラスを新規作成し、IActorBrainインターフェースを実装

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/Brains/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Threading;
+
+namespace SoulLike.ActorControllers.Brains
+{
+    public sealed class Enemy : IActorBrain
+    {
+        public Enemy()
+        {
+        }
+
+        public void Attach(Actor actor, CancellationToken cancellationToken)
+        {
+        }
+    }
+}
