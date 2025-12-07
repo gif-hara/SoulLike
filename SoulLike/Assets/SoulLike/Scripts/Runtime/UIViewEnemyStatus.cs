b6c1d5950689182e52c1a82b8f23656c77003dc7
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-07 23:06:53 +0900

    UIViewEnemyStatusクラスを新規作成し、敵のヒットポイントをスライダーにバインドする機能を追加しました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UIViewEnemyStatus.cs
@@ -0,0 +1,29 @@
+using R3;
+using SoulLike.ActorControllers;
+using SoulLike.ActorControllers.Abilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoulLike
+{
+    public class UIViewEnemyStatus : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider hitPointSlider;
+
+        public void Bind(Actor actor)
+        {
+            var actorStatus = actor.GetAbility<ActorStatus>();
+            Observable.Merge(
+                actorStatus.HitPointMax,
+                actorStatus.HitPoint
+            )
+                .Subscribe((this, actorStatus), static (_, t) =>
+                {
+                    var (@this, actorStatus) = t;
+                    @this.hitPointSlider.value = actorStatus.HitPointRate;
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+        }
+    }
+}
