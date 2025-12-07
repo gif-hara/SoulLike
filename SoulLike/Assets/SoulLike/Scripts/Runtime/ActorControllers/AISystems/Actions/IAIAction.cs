6ba680a49bf300c0e47a3ad90d5012973a599657
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-07 16:10:56 +0900

    IAIActionインターフェースを新規作成し、AIアクションの非同期処理を定義しました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/AISystems/Actions/IAIAction.cs
@@ -0,0 +1,10 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace SoulLike.ActorControllers.AISystems
+{
+    public interface IAIAction
+    {
+        UniTask InvokeAsync(Actor actor, CancellationToken cancellationToken);
+    }
+}
