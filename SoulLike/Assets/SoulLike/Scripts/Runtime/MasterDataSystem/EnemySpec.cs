3ec6ef9c5e94f756790aecca449db4651a0b558a
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-07 15:18:15 +0900

    EnemySpecクラスを新規作成し、敵のステータスを管理するプロパティを追加しました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/MasterDataSystem/EnemySpec.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace SoulLike.MasterDataSystem
+{
+    [System.Serializable]
+    public sealed class EnemySpec
+    {
+        [field: SerializeField]
+        public ActorStatusSpec ActorStatusSpec { get; private set; }
+
+        [field: SerializeField]
+        public float MoveSpeed { get; private set; }
+
+        [field: SerializeField]
+        public float RotateSpeed { get; private set; }
+
+        [field: SerializeField]
+        public Weapon WeaponPrefab { get; private set; }
+    }
+}
