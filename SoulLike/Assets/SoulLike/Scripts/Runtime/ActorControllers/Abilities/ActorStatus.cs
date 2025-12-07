24b441ffb3707df1da52647fb74a3798892f2c4e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-07 14:58:21 +0900

    ActorStatusクラスにHitPointおよびHitPointMaxプロパティを追加し、ApplySpecメソッドを実装しました。

--- SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Abilities/ActorStatus.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Abilities/ActorStatus.cs
@@ -1,3 +1,4 @@
+using R3;
 using UnityEngine;
 
 namespace SoulLike.ActorControllers.Abilities
@@ -10,11 +11,25 @@ namespace SoulLike.ActorControllers.Abilities
 
         private ActorAnimation actorAnimation;
 
+        private ReactiveProperty<float> hitPoint = new();
+
+        private ReactiveProperty<float> hitPointMax = new();
+
+        public ReadOnlyReactiveProperty<float> HitPoint => hitPoint;
+
+        public ReadOnlyReactiveProperty<float> HitPointMax => hitPointMax;
+
         public void Activate(Actor actor)
         {
             this.actor = actor;
             actorMovement = actor.GetAbility<ActorMovement>();
             actorAnimation = actor.GetAbility<ActorAnimation>();
+        }
+
+        public void ApplySpec(MasterDataSystem.ActorStatusSpec spec)
+        {
+            hitPointMax.Value = spec.HitPoint;
+            hitPoint.Value = spec.HitPoint;
         }
 
         public void TakeDamage(Actor attacker, AttackData attackData)
