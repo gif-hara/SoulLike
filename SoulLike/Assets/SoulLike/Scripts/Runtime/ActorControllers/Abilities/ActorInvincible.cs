9f52a28e94d131d01de11dc42c315245c35ed779
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-07 18:24:16 +0900

    ActorInvincibleクラスを新規作成し、無敵状態の処理を実装しました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Abilities/ActorInvincible.cs
@@ -0,0 +1,37 @@
+using R3;
+
+namespace SoulLike.ActorControllers.Abilities
+{
+    public class ActorInvincible : IActorAbility
+    {
+        private Actor actor;
+
+        private Blocker invincibleBlocker = new();
+
+        private ReactiveProperty<bool> isInvincible = new(false);
+
+        public ReadOnlyReactiveProperty<bool> IsInvincible => isInvincible;
+
+        public void Activate(Actor actor)
+        {
+            this.actor = actor;
+        }
+
+        public void BeginInvincible(string topic)
+        {
+            invincibleBlocker.Block(topic);
+            isInvincible.Value = true;
+            actor.HitCollider.enabled = false;
+        }
+
+        public void EndInvincible(string topic)
+        {
+            invincibleBlocker.Unblock(topic);
+            if (!invincibleBlocker.IsBlocked)
+            {
+                isInvincible.Value = false;
+                actor.HitCollider.enabled = true;
+            }
+        }
+    }
+}
