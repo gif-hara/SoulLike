2fe417e1593b805dcfeaf8810f397be18c0ca8ab
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 00:16:59 +0900

    UserDataクラスの経験値プロパティをReactivePropertyに変更し、追加メソッドを実装しました。

--- SoulLike/Assets/SoulLike/Scripts/Runtime/UserData.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UserData.cs
@@ -1,9 +1,17 @@
+using R3;
 using UnityEngine;
 
 namespace SoulLike
 {
     public class UserData
     {
-        public int Experience;
+        private ReactiveProperty<int> experience = new(0);
+
+        public ReadOnlyReactiveProperty<int> Experience => experience;
+
+        public void AddExperience(int amount)
+        {
+            experience.Value += amount;
+        }
     }
 }
