18503ebd3746e7de410b03ef0f543a4b1d6a8e4b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 00:42:02 +0900

    UIViewPlayerStatusにUserDataを追加し、経験値をUIに表示する機能を実装しました。

--- SoulLike/Assets/SoulLike/Scripts/Runtime/UIViewPlayerStatus.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UIViewPlayerStatus.cs
@@ -1,6 +1,7 @@
 using R3;
 using SoulLike.ActorControllers;
 using SoulLike.ActorControllers.Abilities;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,7 +15,10 @@ namespace SoulLike
         [SerializeField]
         private Slider staminaSlider;
 
-        public void Bind(Actor actor)
+        [SerializeField]
+        private TMP_Text experienceText;
+
+        public void Bind(Actor actor, UserData userData)
         {
             var actorStatus = actor.GetAbility<ActorStatus>();
             Observable.Merge(
@@ -35,6 +39,12 @@ namespace SoulLike
                 {
                     var (@this, actorStatus) = t;
                     @this.staminaSlider.value = actorStatus.StaminaRate;
+                })
+                .RegisterTo(actor.destroyCancellationToken);
+            userData.Experience
+                .Subscribe(this, static (x, @this) =>
+                {
+                    @this.experienceText.text = x.ToString();
                 })
                 .RegisterTo(actor.destroyCancellationToken);
         }
