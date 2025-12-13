206cd542b73e2e3a370253749fa6a5a71286ea8d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 22:53:39 +0900

    AddParameterクラスを新規作成し、IShopActionインターフェースを実装

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ShopActions/AddParameter.cs
@@ -0,0 +1,10 @@
+namespace SoulLike.ShopActions
+{
+    public class AddParameter : IShopAction
+    {
+        public void Invoke(UserData userData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
