1970a3be4f56ffb94daca6b9e86a24022a197adf
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 13:48:09 +0900

    IAdditionalStatusインターフェースの実装クラスAdditionalStatusEmptyを追加しました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/AdditionalStatusEmpty.cs
@@ -0,0 +1,15 @@
+namespace SoulLike.ActorControllers
+{
+    public sealed class AdditionalStatusEmpty : IAdditionalStatus
+    {
+        public int HitPoint => 0;
+
+        public int Stamina => 0;
+
+        public float StaminaRecoveryPerSecond => 0.0f;
+
+        public float AttackRate => 1f;
+
+        public float DefenseRate => 1f;
+    }
+}
