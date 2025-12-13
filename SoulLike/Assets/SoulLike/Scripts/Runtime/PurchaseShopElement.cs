852e58415b19c286fc8cbac5ac8d0cfe2cfd3eb8
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 23:38:19 +0900

    PurchaseShopElementクラスを新規作成し、ショップ要素のIDと価格インデックスを追加

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/PurchaseShopElement.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SoulLike
+{
+    [Serializable]
+    public sealed class PurchaseShopElement
+    {
+        public string ShopElementId;
+
+        public int PriceIndex;
+    }
+}
