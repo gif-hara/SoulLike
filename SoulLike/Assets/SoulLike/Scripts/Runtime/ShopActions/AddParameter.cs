9da9de83741e5175c24ab5fb4f5c9fdc62df5ee6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 22:57:26 +0900

    AddParameterクラスのInvokeメソッドを実装し、パラメータに応じたユーザーデータの更新を追加

--- SoulLike/Assets/SoulLike/Scripts/Runtime/ShopActions/AddParameter.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ShopActions/AddParameter.cs
@@ -1,10 +1,44 @@
+using UnityEngine;
+
 namespace SoulLike.ShopActions
 {
     public class AddParameter : IShopAction
     {
+        public enum ParameterType
+        {
+            HitPoint,
+            Stamina,
+            StaminaRecoveryPerSecond,
+            AttackRate,
+            DamageCutRate
+        }
+
+        [SerializeField]
+        private ParameterType parameterType;
+
+        [SerializeField]
+        private float amount;
+
         public void Invoke(UserData userData)
         {
-            throw new System.NotImplementedException();
+            switch (parameterType)
+            {
+                case ParameterType.HitPoint:
+                    userData.AddHitPoint((int)amount);
+                    break;
+                case ParameterType.Stamina:
+                    userData.AddStamina((int)amount);
+                    break;
+                case ParameterType.StaminaRecoveryPerSecond:
+                    userData.AddStaminaRecoveryPerSecond(amount);
+                    break;
+                case ParameterType.AttackRate:
+                    userData.AddAttackRate(amount);
+                    break;
+                case ParameterType.DamageCutRate:
+                    userData.AddDamageCutRate(amount);
+                    break;
+            }
         }
     }
 }
