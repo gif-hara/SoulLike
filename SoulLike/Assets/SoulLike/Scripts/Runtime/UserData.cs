b96aa4f99a1e73ebf11c751d33c738abdff3049e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 22:56:18 +0900

    UserDataクラスにIAdditionalStatusインターフェースの実装を追加し、プロパティをreadonlyに変更

--- SoulLike/Assets/SoulLike/Scripts/Runtime/UserData.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UserData.cs
@@ -1,13 +1,43 @@
 using R3;
-using UnityEngine;
+using SoulLike.ActorControllers;
 
 namespace SoulLike
 {
-    public class UserData
+    public class UserData : IAdditionalStatus
     {
-        private ReactiveProperty<int> experience = new(0);
+        private readonly ReactiveProperty<int> experience = new(0);
 
         public ReadOnlyReactiveProperty<int> Experience => experience;
+
+        private readonly ReactiveProperty<int> hitPoint = new();
+
+        public ReadOnlyReactiveProperty<int> HitPoint => hitPoint;
+
+        private readonly ReactiveProperty<int> stamina = new();
+
+        public ReadOnlyReactiveProperty<int> Stamina => stamina;
+
+        private readonly ReactiveProperty<float> staminaRecoveryPerSecond = new();
+
+        public ReadOnlyReactiveProperty<float> StaminaRecoveryPerSecond => staminaRecoveryPerSecond;
+
+        private readonly ReactiveProperty<float> attackRate = new();
+
+        public ReadOnlyReactiveProperty<float> AttackRate => attackRate;
+
+        private readonly ReactiveProperty<float> damageCutRate = new();
+
+        public ReadOnlyReactiveProperty<float> DamageCutRate => damageCutRate;
+
+        int IAdditionalStatus.HitPoint => hitPoint.Value;
+
+        int IAdditionalStatus.Stamina => stamina.Value;
+
+        float IAdditionalStatus.StaminaRecoveryPerSecond => staminaRecoveryPerSecond.Value;
+
+        float IAdditionalStatus.AttackRate => attackRate.Value;
+
+        float IAdditionalStatus.DamageCutRate => damageCutRate.Value;
 
         public void AddExperience(int amount)
         {
