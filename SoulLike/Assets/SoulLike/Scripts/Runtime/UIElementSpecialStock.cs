abfe8e2cf17a4a7cc535c451fc5797264e9af5ea
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-13 15:09:28 +0900

    UIViewSpecialStockElementクラスにactiveObjectとinactiveObjectを追加し、SetActiveメソッドを実装

--- SoulLike/Assets/SoulLike/Scripts/Runtime/UIElementSpecialStock.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UIElementSpecialStock.cs
@@ -4,5 +4,16 @@ namespace SoulLike
 {
     public class UIViewSpecialStockElement : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject activeObject;
+
+        [SerializeField]
+        private GameObject inactiveObject;
+
+        public void SetActive(bool isActive)
+        {
+            activeObject.SetActive(isActive);
+            inactiveObject.SetActive(!isActive);
+        }
     }
 }
