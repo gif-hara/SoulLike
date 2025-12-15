446a0f1ac6d770393147d21c3c763576a572fd16
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-15 11:47:11 +0900

    UIViewDialogにメッセージ表示機能を追加し、初期化メソッドを実装

--- SoulLike/Assets/SoulLike/Scripts/Runtime/UIViewDialog.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UIViewDialog.cs
@@ -2,12 +2,16 @@ using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using R3;
+using TMPro;
 using UnityEngine;
 
 namespace SoulLike
 {
     public class UIViewDialog : MonoBehaviour
     {
+        [SerializeField]
+        private TMP_Text message;
+
         [SerializeField]
         private Transform buttonParent;
 
@@ -16,14 +20,22 @@ namespace SoulLike
 
         private List<UIElementButton> buttonInstances = new();
 
+        public void Initialize()
+        {
+            gameObject.SetActive(false);
+        }
+
         public async UniTask<int> ShowAsync(string message, string[] options)
         {
+            this.message.SetText(message);
             for (int i = 0; i < options.Length; i++)
             {
                 var buttonInstance = Instantiate(uiElementButtonPrefab, buttonParent);
                 buttonInstance.Text.SetText(options[i]);
                 buttonInstances.Add(buttonInstance);
             }
+            buttonInstances.Select(x => x.Button).ToList().SetNavigationHorizontal();
+            buttonInstances[0].Button.Select();
             gameObject.SetActive(true);
             var result = await UniTask.WhenAny(buttonInstances.Select(x => x.Button.OnClickAsync()));
             gameObject.SetActive(false);
