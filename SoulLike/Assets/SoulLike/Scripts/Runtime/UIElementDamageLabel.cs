d418a0be541932d87cd38ce768f943257c847b78
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-17 00:21:11 +0900

    UIElementDamageLabelに非同期セットアップ処理を追加し、表示遅延を設定できるようにしました。

--- SoulLike/Assets/SoulLike/Scripts/Runtime/UIElementDamageLabel.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UIElementDamageLabel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
@@ -11,11 +14,22 @@ namespace SoulLike
         [SerializeField]
         private TMP_Text label;
 
+        [SerializeField]
+        private float destroyDelay = 1.0f;
+
         public void Setup(string text, Vector3 worldPosition, Camera worldCamera)
+        {
+            SetupAsync(text, worldPosition, worldCamera, destroyCancellationToken).Forget();
+        }
+
+        private async UniTask SetupAsync(string text, Vector3 worldPosition, Camera worldCamera, CancellationToken cancellationToken)
         {
             label.text = text;
             var screenPosition = worldCamera.WorldToScreenPoint(worldPosition);
             labelRoot.position = screenPosition;
+
+            await UniTask.Delay(TimeSpan.FromSeconds(destroyDelay), cancellationToken: cancellationToken);
+            Destroy(gameObject);
         }
     }
 }
