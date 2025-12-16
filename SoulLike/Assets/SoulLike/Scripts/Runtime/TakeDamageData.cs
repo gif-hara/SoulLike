2b440e3d9cd52790b6912f403d25888b5361784e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-16 21:55:10 +0900

    TakeDamageData構造体を新規作成し、ダメージ情報を保持できるようにしました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/TakeDamageData.cs
@@ -0,0 +1,12 @@
+namespace SoulLike
+{
+    public readonly struct TakeDamageData
+    {
+        public readonly int Damage;
+
+        public TakeDamageData(int damage)
+        {
+            Damage = damage;
+        }
+    }
+}
