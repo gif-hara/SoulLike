cca08b50da0dd599f76e6fc81c30e4168d3661ea
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-19 23:25:52 +0900

    Projectileクラスを新規作成し、攻撃時の処理を実装しました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/Projectile.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using HK;
+using R3;
+using R3.Triggers;
+using SoulLike.ActorControllers;
+using SoulLike.ActorControllers.Abilities;
+using UnityEngine;
+
+namespace SoulLike
+{
+    public sealed class Projectile : MonoBehaviour
+    {
+        [SerializeField]
+        private Collider controlledCollider;
+
+        private readonly HashSet<Actor> hitActors = new();
+
+        public IDisposable Activate(Actor actor, AttackData attackData, List<GameObject> hitEffectPrefabs)
+        {
+            hitActors.Clear();
+            return controlledCollider.OnTriggerStayAsObservable()
+                .Subscribe((this, actor, attackData, hitEffectPrefabs), static (x, t) =>
+                {
+                    var (@this, actor, attackData, hitEffectPrefabs) = t;
+                    var target = x.attachedRigidbody?.GetComponent<Actor>();
+                    if (target == null)
+                    {
+                        return;
+                    }
+                    if (@this.hitActors.Contains(target))
+                    {
+                        return;
+                    }
+                    var targetStatus = target.GetAbility<ActorStatus>();
+                    if (targetStatus == null)
+                    {
+                        return;
+                    }
+                    if (targetStatus.IsInvincible.CurrentValue)
+                    {
+                        return;
+                    }
+                    if (targetStatus.IsParrying)
+                    {
+                        target.Event.Broker.Publish(new ActorEvent.OnBeginParry());
+                    }
+                    else
+                    {
+                        var takeDamageData = targetStatus.TakeDamage(actor, attackData);
+                        var hitPosition = x.ClosestPoint(attackData.Collider.transform.position);
+                        actor.GetAbility<ActorStatus>().AddSpecialPower(attackData.SpecialPowerRecovery);
+                        actor.Event.Broker.Publish(new ActorEvent.OnGiveDamage(attackData, targetStatus.IsStunned, takeDamageData.Damage, hitPosition));
+                        if (!string.IsNullOrEmpty(attackData.SfxKey))
+                        {
+                            TinyServiceLocator.Resolve<AudioManager>().PlaySfx(attackData.SfxKey);
+                        }
+                        if (targetStatus.IsStunned)
+                        {
+                            if (!string.IsNullOrEmpty(attackData.SfxKeyOnStun))
+                            {
+                                TinyServiceLocator.Resolve<AudioManager>().PlaySfx(attackData.SfxKeyOnStun);
+                            }
+                        }
+                        foreach (var hitEffectPrefab in hitEffectPrefabs)
+                        {
+                            Instantiate(hitEffectPrefab, hitPosition, Quaternion.identity);
+                        }
+                    }
+                    @this.hitActors.Add(target);
+                });
+        }
+    }
+}
