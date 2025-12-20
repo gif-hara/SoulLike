9a8e275a5685979a54166361895ef4f537cfc7cf
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-20 12:29:58 +0900

    プレイヤーの死亡時にBGMをフェードアウトさせる処理を追加しました。

--- SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Brains/Player.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Brains/Player.cs
@@ -153,11 +153,11 @@ namespace SoulLike.ActorControllers.Brains
                     @this.userData.DeadCount++;
                     @this.worldCameraController.PlayOnDeadImpulse1();
                     var audioManager = TinyServiceLocator.Resolve<AudioManager>();
-                    audioManager.StopBgm();
                     audioManager.PlaySfx("BeginStun.1");
                     await HK.Time.Root.BeginHitStopAsync(1.0f, 0.0f, cts);
                     @this.worldCameraController.PlayLockOnCameraNoiseAnimationAsync(10.0f, 0.0f, 3.0f, cts).Forget();
                     audioManager.PlaySfx("Defeat.1");
+                    audioManager.FadeOutBgmAsync(2.0f, cts).Forget();
                     await HK.Time.Root.BeginHitStopAsync(1.0f, 0.3f, cts);
                     @this.sceneBroker.Publish(new MainSceneEvent.GameJudgement(MainSceneEvent.JudgementType.PlayerLose));
                 })
