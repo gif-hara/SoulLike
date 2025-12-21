064920eb32b928c0feecc865696e3f5c3368504f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-21 15:10:59 +0900

    ActorEffectクラスを新規作成し、エフェクト管理機能を実装しました

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/ActorControllers/Abilities/ActorEffect.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoulLike.ActorControllers.Abilities
+{
+    public sealed class ActorEffect : IActorAbility
+    {
+        private Actor actor;
+
+        private readonly Dictionary<string, Element> elements = new();
+
+        public void Activate(Actor actor)
+        {
+            this.actor = actor;
+            foreach (var effectObject in actor.EffectObjects)
+            {
+                elements.Add(effectObject.name, new Element(effectObject));
+            }
+        }
+
+        public void SetActive(string effectName, bool active)
+        {
+            if (elements.TryGetValue(effectName, out var element))
+            {
+                element.SetActive(active);
+            }
+        }
+
+        public void Reset()
+        {
+            foreach (var element in elements.Values)
+            {
+                element.Reset();
+            }
+        }
+
+        public class Element
+        {
+            private readonly GameObject effectObject;
+
+            private readonly bool initialActive;
+
+            public Element(GameObject effectObject)
+            {
+                this.effectObject = effectObject;
+                initialActive = effectObject.activeSelf;
+            }
+
+            public void SetActive(bool active)
+            {
+                effectObject.SetActive(active);
+            }
+
+            public void Reset()
+            {
+                effectObject.SetActive(initialActive);
+            }
+        }
+    }
+}
