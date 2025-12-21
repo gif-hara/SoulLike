74901e9461570df9de8af44ba995361f7fe640cb
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-21 22:53:15 +0900

    InputSpriteクラスを新規作成し、入力アクションに基づくスプライト名を取得する機能を追加しました。

---
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/InputSprite.cs
@@ -0,0 +1,85 @@
+using System.Linq;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
+#if !UNITY_WEBGL
+using UnityEngine.InputSystem.Switch;
+#endif
+using UnityEngine.InputSystem.XInput;
+
+namespace HK
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class InputSprite
+    {
+        public static string GetTag(InputAction action, InputScheme inputScheme)
+        {
+            var schemeName = inputScheme.CurrentInputSchemeType switch
+            {
+                InputScheme.InputSchemeType.KeyboardAndMouse => "Keyboard&Mouse",
+                InputScheme.InputSchemeType.GamePad => "Gamepad",
+                _ => "Unknown"
+            };
+
+            var bindingMask = InputBinding.MaskByGroup(schemeName);
+            var sb = new System.Text.StringBuilder();
+
+            foreach (var binding in action.bindings)
+            {
+                if (!bindingMask.Matches(binding))
+                {
+                    continue;
+                }
+                var path = binding.effectivePath;
+                var matchedControls = action.controls.Where(x => InputControlPath.Matches(path, x));
+                foreach (var control in matchedControls)
+                {
+                    if (control is InputDevice)
+                    {
+                        continue;
+                    }
+                    if (control.device is Gamepad && inputScheme.CurrentGamepad != null)
+                    {
+                        if (control.device != inputScheme.CurrentGamepad)
+                        {
+                            continue;
+                        }
+                    }
+                    sb.Append($"<sprite name={GetSpriteName(control)}>");
+                }
+            }
+
+            if (sb.Length > 0)
+            {
+                return sb.ToString();
+            }
+            else
+            {
+                return $"<sprite=UnknownTag schemeName:{schemeName} action:{action.name}>";
+            }
+        }
+
+        public static string GetTag(InputAction action)
+        {
+            return GetTag(action, TinyServiceLocator.Resolve<InputScheme>());
+        }
+
+        public static string GetSpriteName(InputControl control)
+        {
+            var deviceIconGroup = control.device switch
+            {
+                Keyboard => "Keyboard",
+                Mouse => "Mouse",
+                XInputController => "XInputController",
+                DualShockGamepad => "DualShockGamepad",
+#if !UNITY_WEBGL
+                SwitchProControllerHID => "SwitchProController",
+#endif
+                _ => "DualShockGamepad"
+            };
+            var controlPathContent = control.path.Substring(control.device.name.Length + 2).Replace('/', '-');
+            return $"{deviceIconGroup}-{controlPathContent}";
+        }
+    }
+}
