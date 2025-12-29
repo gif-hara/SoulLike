510b2f0ee26e63c638cb844fdd88348928acaebb
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-12-29 23:22:40 +0900

    BeginAsyncメソッドにUserDataとAudioManagerを追加し、スライダーの初期値設定と音量変更処理を実装しました。

--- SoulLike/Assets/SoulLike/Scripts/Runtime/UIViewTitle.cs
+++ SoulLike/Assets/SoulLike/Scripts/Runtime/UIViewTitle.cs
@@ -1,5 +1,7 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using HK;
+using R3;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -17,9 +19,30 @@ namespace SoulLike
         [SerializeField]
         private Slider sfxSlider;
 
-        public UniTask BeginAsync(CancellationToken cancellationToken)
+        public UniTask BeginAsync(UserData userData, AudioManager audioManager, CancellationToken cancellationToken)
         {
             EventSystem.current.SetSelectedGameObject(startButton.gameObject);
+            bgmSlider.value = userData.bgmVolume.Value;
+            sfxSlider.value = userData.sfxVolume.Value;
+            bgmSlider.OnValueChangedAsObservable()
+                .Skip(1)
+                .Subscribe((userData, audioManager), static (x, state) =>
+                {
+                    var (userData, audioManager) = state;
+                    userData.bgmVolume.Value = x;
+                    audioManager.SetVolumeBgm(x);
+                })
+                .RegisterTo(cancellationToken);
+            sfxSlider.OnValueChangedAsObservable()
+                .Skip(1)
+                .Subscribe((userData, audioManager), static (x, state) =>
+                {
+                    var (userData, audioManager) = state;
+                    userData.sfxVolume.Value = x;
+                    audioManager.SetVolumeSfx(x);
+                    audioManager.PlaySfx("Parry.Success.1");
+                })
+                .RegisterTo(cancellationToken);
             return startButton.OnClickAsync(cancellationToken);
         }
 
